179d6e3cc20748934e3e686acc943d1699a7b735
wendellast <wendelalves01a@hotmail.com>
2025-03-26 19:16:20 -0300

    logo-eduga

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from fastapi import (
     Request,
 )
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 from middleware.cors import add_cors_middleware
 from routes import auth, fases, perguntas, progress, temas, users
 from settings.database import Base, engine
@@ -17,6 +19,9 @@ app = FastAPI(title="Quiz Game API")
 add_cors_middleware(app)
 
 templates = Jinja2Templates(directory="templates")
+
+
+app.mount("/static", StaticFiles(directory="templates/static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
